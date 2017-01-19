247442e5a19dbcdd15f6a038729db25c6e32d698
Matt Kelley and Sarah Leahy <matt@>
2017-01-19 13:44:36 -0800

    make a loop to grab the corect values of the array and output them with no spaces

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,34 @@
-var answer = ""
-var inputValue
+var answer = [];
+var inputValue;
 
 function encode(letters){
-  var splt = letters.split("");
+  var noSpace = letters.replace(/\s/g,"");
+  var splt = noSpace.split("");
   var lngth = splt.length;
-  var sqrd = Math.pow(Math.round(Math.sqrt(lngth)),2);
-  console.log("Length of the string is: " + lngth);
-  console.log("Area of the grid is: " + sqrd);
+  var sqroot = Math.round(Math.sqrt(lngth))
+  var sqrd = Math.pow(sqroot,2);
+  var width;
+  var height;
 
-  if (sqrd > lngth){
-    answer = "yay";
+  if (sqrd >= lngth){
+    width = sqroot;
+    height = sqroot;
   } else{
-    answer = "booo"
+    width = sqroot + 1;
+    height = sqroot;
+  }
+  console.log("Area of the grid is: " + sqrd);
+  console.log("number of values in array " + lngth);
+  console.log("height of the array is " + height);
+  console.log("the split returns " + splt);
+  console.log("the width if the array is " + width);
+  console.log("Width times height is: " + width*height);
+
+  for(i=0;i<width;i++){
+    for (j=i; j<lngth; j+=width){
+      answer.push(splt[j]);
+      console.log("The letter in this loop is: " + splt[j]);
+    }
   }
 }
 
